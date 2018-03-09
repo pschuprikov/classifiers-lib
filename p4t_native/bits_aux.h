9284cbaee0384a7a5f58e6e19e52805195691670
Pavel Chuprikov <pschuprikov@gmail.com>
2018-03-09 21:14:55 +0100

    enable 128bit filters

---
+++ p4t_native/bits_aux.h
@@ -0,0 +1,94 @@
+#ifndef BITS_AUX_H
+#define BITS_AUX_H
+
+#include "common.h"
+#include "hashing_aux.h"
+
+typedef long long int u2 __attribute__ ((vector_size(16)));
+
+namespace std {
+
+template<>
+struct hash<u2> {
+    auto operator()(u2 const& x) const -> size_t {
+        using comp = decltype(std::declval<u2>()[0]);
+        size_t result = 0;
+        p4t::hash::hash_combine(result, std::hash<comp>()(x[0]));
+        p4t::hash::hash_combine(result, std::hash<comp>()(x[1]));
+        return result;
+    }
+};
+
+}
+
+namespace p4t {
+
+namespace num {
+
+inline auto testz(__uint128_t const &x) -> bool {
+    return x == 0;
+}
+
+inline auto ctz(__uint128_t const& x) -> bool {
+    return __builtin_ctz(x);
+}
+
+inline auto testz(u2 const &x) -> bool {
+    const u2 zero = {0, 0};
+    return __builtin_ia32_ptestz128(x, ~zero);
+}
+
+inline auto ctz(u2 const &x) -> size_t {
+    if (x[0] == 0) {
+        return sizeof(x[0]) * 8 + __builtin_ctz(x[1]);
+    } else {
+        return __builtin_ctz(x[0]);
+    }
+}
+
+inline auto testz(uint64_t const &x) -> bool {
+    return x == 0;
+}
+
+inline auto ctz(uint64_t const& x) -> bool {
+    return __builtin_ctz(x);
+}
+
+}
+
+template<class T> struct kth_bit {
+    static auto value(size_t k) -> T = delete;
+};
+
+template<>
+struct kth_bit<u2> {
+    static auto value(size_t k) -> u2 {
+        using comp = decltype(std::declval<u2>()[0]);
+        if (k < sizeof(comp) * 8) {
+            u2 const result = {0, comp(1) << k};
+            return result;
+        } else {
+            u2 const result = {comp(1) << (k - sizeof(comp) * 8), 0};
+            return result;
+        }
+    }
+};
+
+template<>
+struct kth_bit<__uint128_t> {
+    static auto value(size_t k) -> __uint128_t {
+        return __uint128_t(1) << k;
+    }
+};
+
+template<>
+struct kth_bit<uint64_t> {
+    static auto value(size_t k) -> uint64_t {
+        return uint64_t(1) << k;
+    }
+};
+
+
+}
+
+#endif
---
+++ p4t_native/hashing_aux.h
@@ -0,0 +1,37 @@
+#ifndef HASHING_AUX_H
+#define HASHING_AUX_H
+
+#include <array>
+
+namespace p4t::hash {
+
+inline auto hash_combine(size_t& result, size_t hash) {
+    result ^= hash + 0x9e3779b9 + (result<<6) + (result>>2);
+}
+
+template <class T, size_t N>
+auto hash_array(std::array<T, N> const& arr) -> size_t {
+    std::size_t result = 0;
+    for (auto i = 0u; i < N; i++) {
+        hash_combine(result, std::hash<T>()(arr[i]));
+    }
+    return result;
+}
+
+template <class T, size_t... idxs>
+auto hash_tuple(T const& t, std::index_sequence<idxs...> const&) -> size_t {
+    std::size_t result = 0;
+    for (auto const& hash: {std::hash<std::tuple_element_t<idxs, T>>()(std::get<idxs>(t))...}) {
+        hash_combine(result, hash);
+    }
+    return result;
+}
+
+template<class... Args> 
+auto hash_tuple(tuple<Args...> const& t) -> size_t {
+    return hash_tuple(t, std::make_index_sequence<sizeof...(Args)>());
+}
+
+}
+
+#endif

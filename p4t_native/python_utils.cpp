8389d307a523800b86a42846214fac1b4c8fc78b
Pavel Chuprikov <pschuprikov@gmail.com>
2018-03-11 23:17:11 +0100

    Speed up from python conversion

--- p4t_native/python_utils.cpp
+++ p4t_native/python_utils.cpp
@@ -1,13 +1,25 @@
 #include "python_utils.h"
 
+#include "timer.h"
+
+#include <boost/python/numpy.hpp>
+
+namespace np = p4t::py::numpy;
+
 auto p4t::svmr_entry2filter(py::object const& entry) -> Filter {
-    auto const width = len(entry.attr("value"));
+    np::ndarray value = np::from_object(entry.attr("value"));
+    np::ndarray mask = np::from_object(entry.attr("mask"));
+
+    bool * const p_value = reinterpret_cast<bool *>(value.get_data()); 
+    bool * const p_mask = reinterpret_cast<bool *>(mask.get_data()); 
+    auto const width = len(value);
+
     Filter result(width);
     for (auto i = 0u; i < width; i++) {
-        if (!entry.attr("mask")[i]) {
+        if (!p_mask[i]) {
             result.set(i, Bit::ANY);
         } else {
-            result.set(i, entry.attr("value")[i] ? Bit::ONE : Bit::ZERO);
+            result.set(i, p_value[i] ? Bit::ONE : Bit::ZERO);
         }
     }
     return result;
@@ -23,6 +35,7 @@ auto p4t::svmr2filters(py::object const& svmr) -> vector<Filter> {
 }
 
 auto p4t::svmr2rules(py::object const& svmr) -> vector<Rule> {
+    Timer t{"from python conversion"};
     vector<Rule> rules{};
     for (auto i = 0; i < len(svmr); i++) {
         Action action;
@@ -37,23 +50,28 @@ auto p4t::svmr2rules(py::object const& svmr) -> vector<Rule> {
 }
 
 auto p4t::rules2svmr(vector<Rule> const& rules) -> py::object {
+    Timer t{"to python conversion"};
     py::list result{};
     for (auto i = 0; i < int(rules.size()); i++) {
-        py::list mask{};
-        py::list value{};
+        np::ndarray mask = np::zeros(
+                py::make_tuple(rules[i].filter().size()), 
+                np::dtype::get_builtin<bool>());
+        np::ndarray value = np::zeros(
+                py::make_tuple(rules[i].filter().size()), 
+                np::dtype::get_builtin<bool>());
         for (auto j = 0; j < int(rules[i].filter().size()); j++) {
             switch(rules[i].filter()[j]) {
                 case Bit::ANY: {
-                    mask.append(false);
-                    value.append(false);
+                    mask[j] = false;
+                    value[j] = false;
                 } break;
                 case Bit::ONE: {
-                    mask.append(true);
-                    value.append(true);
+                    mask[j] = true;
+                    value[j] = true;
                 } break;
                 case Bit::ZERO: {
-                    mask.append(true);
-                    value.append(false);
+                    mask[j] = true;
+                    value[j] = false;
                 } break;
             }
         }
